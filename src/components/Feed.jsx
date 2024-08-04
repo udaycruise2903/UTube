3ca3b96898b0daccbc625201e1cb06c11deec862
udaycruise2903 <udaycruise2903@gmail.com>
2024-08-04 18:53:48 +0530

    feat: implement sidebar, setup videos

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import { SideBar, Videos } from './';
+
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
 
-export default Feed
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
+    // return () => {
+      
+    // }
+  }, [selectedCategory]);
+  
+
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" }}}>
+      <Box sx={{ height: { sx: 'auto', md: '92vh', px: { sx: 0, md: 2} }}}>
+        <SideBar 
+          selectedCategory={selectedCategory} 
+          setSelectedCategory={setSelectedCategory}
+          /> 
+
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff'  }}> 
+          Copyright 2024 Ud Media
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white' }}>
+          {selectedCategory} <span style={{ color: '#F31503' }}>videos
+            <Videos videos={videos}/>
+          </span>
+        </Typography>
+      </Box>
+    </Stack>
+  )
+};
+
+export default Feed;
