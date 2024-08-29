86ae4f8fc34f7805c4126f8d7cd01902f59ca5fb
udaycruise2903 <udaycruise2903@gmail.com>
2024-08-29 16:39:53 +0530

    feat: setup channel

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import { Videos, ChannelCard } from './'
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null)
+  const [videos, setVideos] = useState([])
+  const { id } = useParams();
+  
+  console.log(channelDetail, videos)
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part="snippet&id=${id}`)
+      .then((data) => setChannelDetail(data?.items[0]));
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
+      .then((data) => setVideos(data?.items));
+  }, [id])
+
   return (
-    <div>ChannelDetail</div>
+    <Box minHeight="95vh">
+      <Box>
+        <div style={{
+          background: 'linear-gradient(90deg, rgba(9,199,237,1) 0%, rgba(244,12,12,1) 50%, rgba(9,199,237,1) 100%)',
+          zIndex: 10,
+          height: '300px'
+        }}          
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+      </Box>
+      <Box display="flex" p="2">
+        <Box sx={{ml: {sm: '160px'}}}>
+        <Videos videos={videos} />
+        </Box> 
+      </Box>
+    </Box>
   )
 }
 
