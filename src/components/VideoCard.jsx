945054c1148899ce7b512b304c061a33959d91d4
udaycruise2903 <udaycruise2903@gmail.com>
2024-08-31 11:33:54 +0530

    fix: Card & CardMedia width

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -15,12 +15,12 @@ const VideoCard = ({ video: { id: {videoId}, snippet } }) => {
   console.log(videoId, snippet);
 
   return (
-    <Card sx={{ width: { md: '320px', xs: '100%'}, boxShadow: 'none', borderRadius: 'none' }}>
+    <Card sx={{ width: { xs: '100%', sm: '358px', md: '320px' }, boxShadow: 'none', borderRadius: 'none' }}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
       <CardMedia 
         image={snippet?.thumbnails?.high?.url}
         alt={snippet?.title}
-        sx = {{ width: 350, height: 180 }}
+        sx = {{ width: { xs: '100%', sm: '358px'}, height: 180 }}
       />
       </Link>
       <CardContent sx = {{ backgroundColor: '#1e1e1e', height: '160px'}}>
